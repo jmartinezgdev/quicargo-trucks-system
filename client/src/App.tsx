07fa8a9b0f69217d121887f610fcfba18aa8a913
Jorge Martinez <Jorge.Martinez@weightwatchers.com>
2021-08-23 11:38:55 -0300

    QUI-Create-Client: Init files

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header>
+        Quicargo Setup Front
+      </header>
+    </div>
+  );
+}
+
+export default App
