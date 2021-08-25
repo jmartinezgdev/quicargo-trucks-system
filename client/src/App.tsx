138f5268243c92ad5888006ba8d8113d3da620d4
Jorge Martinez <Jorge.Martinez@weightwatchers.com>
2021-08-25 03:58:30 -0300

    fix naming

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { PageNotFound } from './common';
-import { CreateLocation, ListLocation } from './components/location';
-import { CreateTruck, ListTruck } from './components/truck';
+import { CreateLocation, ListLocation } from './components/Location';
+import { CreateTruck, ListTruck } from './components/Truck';
 import { Header } from './layout';
 
 const App: React.FC = () => {
